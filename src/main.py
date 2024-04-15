644d32122e3577d11c02bcbf2403fafa91a58c47
Bracerr <89707169+Bracerr@users.noreply.github.com>
2024-04-15 19:56:43 +0300

    update sequence array

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # Параметры моделирования
 sigma_0_sq = 0.25
-rho_1_values = [0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
+rho_1_values = [0.5, 0.55, 0.6, 0.65 , 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
 num_steps = 100000
 
 def generate_normal():
